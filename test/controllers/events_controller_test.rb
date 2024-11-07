f98a65352272484cd3f66dab5c44ed032b8f11f2
MoralesWilll <will1ams.moralesm22@gmail.com>
2024-11-07 09:07:03 -0500

    fix: updated the test params to meet expected validations

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -17,7 +17,7 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create event" do
     assert_difference("Event.count") do
-      post events_url, params: { event: { date: @event.date, description: @event.description, name: @event.name } }
+      post events_url, params: { event: { date: 1.day.from_now, description: "Updated description", name: "Updated Event Name" } }
     end
 
     assert_redirected_to event_url(Event.last)
@@ -34,7 +34,7 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update event" do
-    patch event_url(@event), params: { event: { date: @event.date, description: @event.description, name: @event.name } }
+    patch event_url(@event), params: { event: { date: 1.day.from_now, description: "Updated description", name: "Updated Event Name" } }
     assert_redirected_to event_url(@event)
   end
 
